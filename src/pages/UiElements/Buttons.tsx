4b0bea1fd48132873fe575aff46bd47a82b116ce
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-08-25 11:03:51 -0300

    Ajustes S2299

--- src/pages/UiElements/Buttons.tsx
+++ src/pages/UiElements/Buttons.tsx
@@ -8,7 +8,7 @@ export default function Buttons() {
   return (
     <div>
       <PageMeta
-        title="React.js Buttons Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        title="ITCD"
         description="This is React.js Buttons Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <PageBreadcrumb pageTitle="Buttons" />

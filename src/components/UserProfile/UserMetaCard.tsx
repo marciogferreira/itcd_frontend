0813ad772d3f1f991fc917b9618f9c552424fdbb
Marcio Ferreira <marcio.ferreira@itarget.com.br>
2025-10-09 08:39:59 -0300

    Configurações de S1202 e Tratamento de Mensagem Rubrica para Desktop

--- src/components/UserProfile/UserMetaCard.tsx
+++ src/components/UserProfile/UserMetaCard.tsx
@@ -27,7 +27,7 @@ export default function UserMetaCard() {
               </h4>
               <div className="flex flex-col items-center gap-1 text-center xl:flex-row xl:gap-3 xl:text-left">
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  {user.role === 1 ? 'Admin' : 'Estudante'}
+                  {user.role == 1 ? 'Admin' : 'Estudante'}
                 </p>
                 <div className="hidden h-3.5 w-px bg-gray-300 dark:bg-gray-700 xl:block"></div>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
